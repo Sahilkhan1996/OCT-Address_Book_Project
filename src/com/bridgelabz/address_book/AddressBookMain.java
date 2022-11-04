b78587fe139f9e8f3d1c3b22da32577f8daa560b
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-04 13:20:55 +0530

    updating program to add contacts in the address boook

--- src/com/bridgelabz/address_book/AddressBookMain.java
+++ src/com/bridgelabz/address_book/AddressBookMain.java
@@ -1,7 +1,50 @@
 package com.bridgelabz.address_book;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
-public static void main(String[] args) {
-	System.out.println("Welcome to Address Book Program");
-}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		// Adding new contacts to address book
+		List<Object> a1 = new ArrayList<>();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of contacts you would like to enter?");
+		int numberOfContacts = sc.nextInt();
+		for (int i = 1; i <= numberOfContacts; i++) {
+			Contacts cs1 = new Contacts();
+			try{System.out.println("Please enter the first name: ");
+			cs1.setFirstName(sc.next());
+			System.out.println("Please enter the last name: ");
+			cs1.setLastName(sc.next());
+			System.out.println("Please enter the Address: ");
+			cs1.setAddress(sc.next());
+			System.out.println("Please enter the city: ");
+			cs1.setCity(sc.next());
+			System.out.println("Please enter the state: ");
+			cs1.setState(sc.next());
+			System.out.println("Please enter the zip: ");
+			cs1.setZip(sc.nextInt());
+			System.out.println("Please enter the Phone Number: ");
+			cs1.setPhoneNumber(sc.nextInt());
+			System.out.println("Please enter the email: ");
+			cs1.setEmail(sc.next());}
+			catch(InputMismatchException e) {
+				System.out.println(e);
+				
+			}
+			// adding the contact in the array list
+			a1.add(cs1);
+			System.out.println(i + " contact are added sucessfully!");
+		}
+		System.out.println("All the contacts are added sucessfully!");
+		for (Object object : a1) {
+			System.out.println(object);
+		}
+		sc.close();
+	}
 }
